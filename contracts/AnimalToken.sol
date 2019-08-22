895f00657b6248de8bf1d70bfb00b7a63edc3605
Kseniya Lifanova <kseniya@upstate.agency>
2019-08-21 17:27:51 -0700

    first commit

---
+++ contracts/AnimalToken.sol
@@ -0,0 +1,8 @@
+pragma solidity ^0.5.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721Full.sol";
+
+contract AnimalToken is ERC721Full {
+
+
+}
---
+++ contracts/DLToken.sol
@@ -0,0 +1,8 @@
+pragma solidity ^0.5.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20Detailed.sol";
+
+contract DLToken is ERC20, ERC20Detailed {
+
+}
